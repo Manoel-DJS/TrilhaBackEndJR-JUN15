906c69cbd0243082f104f12eb2e39f50c8ee04fe
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-07-10 16:13:11 -0300

    Create UserService

---
+++ src/main/java/cc/login/tasksystem/service/UserService.java
@@ -0,0 +1,38 @@
+package cc.login.tasksystem.service;
+
+
+import cc.login.tasksystem.controllers.dto.CreateUserDto;
+import cc.login.tasksystem.models.User;
+import cc.login.tasksystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public UUID createUser(CreateUserDto userDto){
+
+        // DTO -> ENTITY
+        var entity = new User();
+        entity.setUserid(UUID.randomUUID());
+        entity.setUsername(userDto.username());
+        entity.setPassword(userDto.password());
+
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserid();
+    }
+
+    public List<User> ListUsers(){
+        return userRepository.findAll();
+    }
+}
