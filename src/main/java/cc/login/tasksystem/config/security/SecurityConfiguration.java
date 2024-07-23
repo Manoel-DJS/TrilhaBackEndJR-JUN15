b9a721de91bec4babb0023135e488eee9b003c6e
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-07-23 11:52:53 -0300

    Visual Permission for Swagger - securityFilterChain

--- src/main/java/cc/login/tasksystem/config/security/SecurityConfiguration.java
+++ src/main/java/cc/login/tasksystem/config/security/SecurityConfiguration.java
@@ -12,12 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
+    private static final String[] PUBLIC_MATCHERS = {"/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**"};
+    // permitir o swagger-ui
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll() // permissão visual
+                        .requestMatchers(HttpMethod.POST, "/end/users").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .build();
     }
 }
