dd174e1647703ac10eb7f0a43b3f1634ac76916f
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-08-16 10:20:18 -0300

    Fixed bug - else troll

--- src/main/java/cc/login/tasksystem/config/security/SecurityFilter.java
+++ src/main/java/cc/login/tasksystem/config/security/SecurityFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,9 +33,9 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } else{
-            filterChain.doFilter(request, response);
         }
+        filterChain.doFilter(request, response);
+
     }
 
     private String recoverToken(HttpServletRequest request){
@@ -46,6 +45,5 @@ public class SecurityFilter extends OncePerRequestFilter {
         } else{
             return authHeader.replace("Bearer ", "");
         }
-
     }
 }
