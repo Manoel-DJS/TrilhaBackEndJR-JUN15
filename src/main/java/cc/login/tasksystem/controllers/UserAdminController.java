56cf811664fd8021d86aea65c313760291531c43
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-08-20 14:07:15 -0300

    Create Controller for admins

---
+++ src/main/java/cc/login/tasksystem/controllers/UserAdminController.java
@@ -0,0 +1,31 @@
+package cc.login.tasksystem.controllers;
+
+import cc.login.tasksystem.models.User;
+import cc.login.tasksystem.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin777")
+@Tag(name = "ADMIN-777", description = "Tarefa")
+public class UserAdminController {
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public ResponseEntity<List<User>> getUserAll(){
+        var users = userService.ListUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteById(@PathVariable("userId") String userId){
+        userService.deleteById(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
